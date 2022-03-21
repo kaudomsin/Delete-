5185da102f2884c1d08f7e20ed80174dfb2cf3d9
Kala <91386800+Udomsink@users.noreply.github.com>
2022-03-21 17:16:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const mySecret = process.env[`TOKEN`]
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+    }
+})
+client.login(process.env.TOKEN);
